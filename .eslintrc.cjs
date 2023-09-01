ca0c0abf00121a40c94882eaf5c5275678bb5d3f
HnatKruk <hnatkivpublic@gmail.com>
2023-09-01 15:38:19 +0300

    Revert "Config eslint"
    This reverts commit 84e0fb12a6258cba7f647bba69ba89ee3b7d3ad3.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "airbnb",
-    'airbnb-typescript',
-    "airbnb/hooks",
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
