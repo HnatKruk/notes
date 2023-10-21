f2de885b6a687d92ad48065c8a6179cee83a7139
HnatKruk <hnatkivpublic@gmail.com>
2023-10-21 14:23:38 +0200

    Refactor using dompurify lib

--- src/components/NoteTextarea/index.tsx
+++ src/components/NoteTextarea/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import DOMPurify from 'dompurify';
 import { editTextActiveNoteRequestAction } from '@actions';
 import { RootStateInterface } from '@interfaces';
+import { ALLOWED_TAGS, ALLOWED_ATTR } from '@/constants/AllowedElements';
 import styles from './styles.module.scss';
 
 interface NoteTextareaProps {
@@ -12,30 +13,69 @@ interface NoteTextareaProps {
 export const NoteTextarea: FC<NoteTextareaProps> = ({ text }) => {
   const dispatch = useDispatch();
   const [textareaValue, setTextareaValue] = useState(text);
+  const [isTextareaFocused, setIsTextareaFocused] = useState(true);
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
   const { isSearchFocus } = useSelector((store: RootStateInterface) => store.viewReducer);
 
+  const setTextareaFocus = () => {
+    if (textareaRef.current && !isSearchFocus) {
+      const { current: textarea } = textareaRef;
+      textarea.focus();
+      textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+    }
+  };
+
+  const handleFocus = () => {
+    setIsTextareaFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsTextareaFocused(false);
+  };
+
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    const sanitizedTextareaValue = DOMPurify.sanitize(event.target.value);
+    const sanitizedTextareaValue = event.target.value;
     setTextareaValue(sanitizedTextareaValue);
     const currentDate = new Date().toISOString();
     dispatch(editTextActiveNoteRequestAction(sanitizedTextareaValue, currentDate));
   };
 
   useEffect(() => {
-    if (textareaRef.current && !isSearchFocus) {
-      const { current: textarea } = textareaRef;
-      textarea.focus();
-      textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+    setTextareaFocus();
+  }, [isSearchFocus, isTextareaFocused]);
+
+  useEffect(() => {
+    if (divRef.current) {
+      const childElements = divRef.current.querySelectorAll('[href]');
+      childElements.forEach((element) => {
+        element.addEventListener('click', (e) => e.stopPropagation());
+      });
     }
-  }, [isSearchFocus]);
+  }, [isTextareaFocused]);
+
+  if (isTextareaFocused) {
+    return (
+      <textarea
+        className={styles.noteTextarea}
+        value={textareaValue}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        ref={textareaRef}
+        onBlur={handleBlur}
+      />
+    );
+  }
 
   return (
-    <textarea
+    <div
+      ref={divRef}
       className={styles.noteTextarea}
-      value={textareaValue}
-      onChange={handleChange}
-      ref={textareaRef}
+      onClick={handleFocus}
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(
+        textareaValue,
+        { ALLOWED_TAGS, ALLOWED_ATTR },
+      )}}
     />
   );
 };
---
+++ src/constants/AllowedElements.ts
@@ -0,0 +1,26 @@
+export const ALLOWED_TAGS: string[] = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'p',
+  'a',
+  'em',
+  'i',
+  'strong',
+  'b',
+  'u',
+  'del',
+  'ins',
+  'ul',
+  'ol',
+  'li',
+  'br',
+];
+
+export const ALLOWED_ATTR: string[] = [
+  'href',
+  'target',
+];
